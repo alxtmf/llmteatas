89711620bfb78895c2006963fbaef7211cc5de4f
alxtmf <tan@sibdigital.net>
2024-04-24 22:19:32 +0800

    Добавляю пример задачи

---
+++ tan/t1/minarr/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Main {
+
+    private static int min (List<Integer> list){
+        Integer min = Integer.MAX_VALUE;
+
+        for(Integer num : list){
+            if(num < min) {
+                min = num;
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(1, 2, 3, -1, 5);
+        Integer rightAnswer = -1;
+
+        Integer answer = min(list);
+
+        if (answer.equals(rightAnswer)) {
+            System.out.println("Тест пройден");
+        }else {
+            System.out.println("Тест не пройден");
+        }
+    }
+}
