d41df2663c94b5999e56d76faa0f6b363832c30f
alxtmf <tan@sibdigital.net>
2024-04-24 22:26:50 +0800

    Добавляю пример задачи

--- tan/t1/minarr/src/Main.java
+++ tan/t1/minarr/src/Main.java
@@ -2,15 +2,17 @@ import java.util.List;
 
 public class Main {
 
-    private static int min (List<Integer> list){
-        Integer min = Integer.MAX_VALUE;
+    private static Integer min (List<Integer> list){
+        Integer min = null;
+        if (list != null &&!list.isEmpty()){
+            min = Integer.MAX_VALUE;
 
-        for(Integer num : list){
-            if(num < min) {
-                min = num;
+            for(Integer num : list){
+                if(num < min) {
+                    min = num;
+                }
             }
         }
-
         return min;
     }
 
