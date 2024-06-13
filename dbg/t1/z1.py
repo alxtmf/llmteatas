f2d4289d6c7173a656072e80ca03a34bab3642a1
Бальжина Доржиева <balyadorzhieva@gmail.com>
2024-06-13 11:38:52 +0300

    task1

--- dbg/t1/z1.py
+++ dbg/t1/z1.py
@@ -1,11 +1,11 @@
-import random
+# "В одномерном массиве, состоящем из N целых элементов, вычислите сумму
+# модулей элементов массива, расположенных после первого элемента, равного 0"
 
-def func(x):
+import random
+import unittest
+
+def func(arr):
     sum=0
-    arr = [0]
-    arr = arr + [random.randint(-10, 10) for i in range(x-1)]
-    random.shuffle(arr)
-    print(arr)
     for i in range(len(arr)):
         if arr[i] == 0:
            t = i
@@ -13,9 +13,24 @@ def func(x):
            for i in range(len(arr2)):
                sum=sum+abs(arr2[i])
            break
-    print(sum)
+    return sum
 
-a = int(input("Введите размер массива"))
-func(a)
 
+
+# a = int(input("Введите размер массива"))
+# arr = [0]
+# arr = arr + [random.randint(-10, 10) for i in range(a-1)]
+# random.shuffle(arr)
+# func(arr)
+
+class TestFuncArray(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(func([1, 2, 4, 0, -3, 7]), 10)
+        self.assertEqual(func([-3, 0, 4, 0, 6, -5]), 15)
+        self.assertEqual(func([1, -3, 0, 4, 6, -3]), 13)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
